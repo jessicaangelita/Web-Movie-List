07ca01ce94423f855a8913f312e2ee14cd77a82a
Dyandra Maheswari <dyandra.m.07@gmail.com>
2023-09-06 13:04:30 +0700

    Update Header
    Clean Code and Adjust Style

--- webmovielist/app/Components/Header.js
+++ webmovielist/app/Components/Header.js
@@ -1,18 +1,17 @@
+import Link from 'next/link'
+
 export default function Header() {
     return (
-        <header class="text-gray-600 body-font">
-            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center bg-green-800">
-                <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                <span class="ml-3 text-xl font-extrabold text-white">21Cineplex</span>
+        <header className=" body-font w-full">
+            <div className="flex flex-wrap mx-auto p-8 flex-col md:flex-row items-center bg-green-800 ">
+                <a className="flex title-font font-medium items-center  mb-4 md:mb-0">
+                <span className="ml-3 text-2xl font-extrabold text-orange-200">21Cineplex</span>
                 </a>
-                <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-                <a class="mr-5 text-white hover:text-gray-900">First Link</a>
-                <a class="mr-5 text-white hover:text-gray-900">Second Link</a>
-                <a class="mr-5 text-white hover:text-gray-900">Third Link</a>
-                <a class="mr-5 text-white hover:text-gray-900">Fourth Link</a>
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400flex flex-wrap items-center text-base justify-center font-medium">
+                    <Link href='/' className="mr-5 text-orange-100 hover:text-orange-200">Movies</Link>
                 </nav>
-                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Sign In
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                <button className="inline-flex items-center bg-orange-200 border-0 py-2 px-4 focus:outline-none hover:bg-orange-300 rounded-lg text-base mt-4 md:mt-0 font-medium">Sign In
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
                 </button>
